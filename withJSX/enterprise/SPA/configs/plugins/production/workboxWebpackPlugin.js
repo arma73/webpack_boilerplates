663b35a152f558d9b9ba636c50d0a68322c066c9
arma73 <arma.biz9805@gmail.com>
2021-02-06 21:31:59 +0400

    fix: place jsdoc

--- withJSX/enterprise/SPA/configs/plugins/production/workboxWebpackPlugin.js
+++ withJSX/enterprise/SPA/configs/plugins/production/workboxWebpackPlugin.js
@@ -3,11 +3,11 @@
 // Core
 const { GenerateSW } = require("workbox-webpack-plugin");
 
-/**
- * The GenerateSW plugin will create a service worker file 
- * for you and add it to the webpack asset pipeline.
- */
 module.exports = () => [
+    /**
+     * The GenerateSW plugin will create a service worker file 
+     * for you and add it to the webpack asset pipeline.
+     */
     new GenerateSW({
         "swDest": "sw.js",
         "include": [/\.html$/, /\.js$/, /\.json$/],

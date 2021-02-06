eaa4d2c25141391b7599b212ac4c0b15d689b7cf
arma73 <arma.biz9805@gmail.com>
2021-02-06 21:38:10 +0400

    add: target path in development config

--- withJSX/enterprise/SPA/configs/webpack/development.config.js
+++ withJSX/enterprise/SPA/configs/webpack/development.config.js
@@ -3,9 +3,13 @@
 // Core
 const { merge } = require("webpack-merge");
 
+// Parts
+const target = require("../parts/development/target");
+
 // Plugins
 const plugins = require("../plugins/development");
 
 module.exports = merge(
+    target,
     plugins
 );

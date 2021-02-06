a8d628e0cb5eecff51216c062d31605aec3b3789
arma73 <arma.biz9805@gmail.com>
2021-02-06 21:36:35 +0400

    add: convert object values to json

---
+++ withJSX/enterprise/SPA/configs/utils/replace.js
@@ -0,0 +1,7 @@
+"use-strict";
+
+exports.convertObjectValuesTOJSON = obj => {
+    const init = {};
+    Object.keys(obj).forEach(item => init[item] = JSON.stringify(obj[item]));
+    return init;
+};
